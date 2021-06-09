7150de5fb95715dd7c7877a981d8d62074e8922d
Isaac Vega <isaacvega1996@gmail.com>
2021-06-09 11:36:29 -0400

    Deploy master

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import reactSvgPlugin from 'vite-plugin-react-svg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-dark-mode/',
   plugins: [ reactRefresh(), reactSvgPlugin() ]
 })
